e92d1231e5fb28a0cb95d38c14fb700d032d1c86
François Granade <fgranade@lmad.eu>
2022-08-10 11:53:40 +0200

    Even more simplifications: same admin customization as in the django documentation

--- multidbbugapp/admin.py
+++ multidbbugapp/admin.py
@@ -1,47 +1,24 @@
-from django.apps import apps
-from django.urls import path
-from django.contrib.admin import AdminSite, ModelAdmin
-
-from django.conf import settings
-
+from django.contrib.admin import ModelAdmin, site
 
 from multidbbugapp.models import Parcel
 
 # See https://docs.djangoproject.com/en/4.0/topics/db/multi-db/#exposing-multiple-databases-in-django-s-admin-interface  # noqa: E501
-class CustomDatabaseModelAdmin(ModelAdmin):
+class MultiDBModelAdmin(ModelAdmin):
+    using = 'other'
+
     def save_model(self, request, obj, form, change):
-        print(f"saving {obj} in {self.admin_site.database}")
-        obj.save(using=self.admin_site.database)
+        obj.save(using=self.using)
 
     def delete_model(self, request, obj):
-        print(f"deleting {obj} in {self.admin_site.database}")
-        obj.delete(using=self.admin_site.database)
+        obj.delete(using=self.using)
 
     def get_queryset(self, request):
-        print(f"querying in {self.admin_site.database}")
-        return super().get_queryset(request).using(self.admin_site.database)
+        return super().get_queryset(request).using(self.using)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        return super().formfield_for_foreignkey(
-            db_field, request, using=self.admin_site.database, **kwargs
-        )
+        return super().formfield_for_foreignkey(db_field, request, using=self.using, **kwargs)
 
     def formfield_for_manytomany(self, db_field, request, **kwargs):
-        return super().formfield_for_manytomany(
-            db_field, request, using=self.admin_site.database, **kwargs
-        )
+        return super().formfield_for_manytomany(db_field, request, using=self.using, **kwargs)
 
-
-class CustomDatabaseAdminSite(AdminSite):
-    def __init__(self, database):
-        super().__init__(database)
-        self.database = database
-        self.register(Parcel, CustomDatabaseModelAdmin)
-
-        self.site_header = "Admin for " + settings.DATABASE_DISPLAY_NAMES[database]
-
-
-admin_urls = [
-    path("admin-" + database + "/", CustomDatabaseAdminSite(database).urls)
-    for database in settings.DATABASES
-]
+site.register(Parcel, MultiDBModelAdmin)
